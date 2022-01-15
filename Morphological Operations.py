59535be9d8d59b54edc73e7a168ade6808646409
Sora Ryu <64963755+sora-ryu@users.noreply.github.com>
2022-01-15 02:34:02 -0500

    Create Morphological Operations.py

---
+++ Morphological Operations.py
@@ -0,0 +1,13 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+# Closing: Dilation -> Erosion, more element, less sharpen
+# the net effect of the closing operation is to remove background pixels that fit the structuring element. In other words, closing tends to close gaps in the image.
+
+# Opening: Erosion -> Dilation, less element, sharpen
+# Morphological opening is an erosion followed by a dilation. The net effect of a morphological opening is to keep only the pixels of the input mask which fits the structuring element. In other words, opening tends to remove pixels or open gaps in the image.
+
+# Closing
+29
+
+# Opening
+20
